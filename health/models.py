0270419f7b6557e9fb2a648cc64dc22ea10ee09e
NellitG <kamaunelson157@gmail.com>
2025-04-24 16:51:29 +0300

    Initial project setup with models for Client, Program, and Enrollment

---
+++ health/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Program(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    
+
+    def __str__(self):
+        return self.name
+    
+class Client(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Enrollment(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='enrollments')
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    enrolled_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.client} -> {self.program}"
