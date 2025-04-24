dd2eca49ccc82e6ca6e210166495798455cbf889
NellitG <kamaunelson157@gmail.com>
2025-04-24 18:39:37 +0300

    register models and createsuperuser

--- health/admin.py
+++ health/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Client, Program, Enrollment
 
 # Register your models here.
+
+#Register Program model
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+
+#Register Client model
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'date_of_birth', 'gender')
+
+#Register Enrollment model
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'program', 'enrolled_on')
+    list_filter = ('program',)
+    search_fields = ('client__first_name', 'program__name')
